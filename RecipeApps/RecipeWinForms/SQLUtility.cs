55abfb6a2db27e12ed21419f274a0a1fe45f3b2b
menuchabern <menucha9021@gmail.com>
2025-05-14 22:58:02 -0400

    setup for recipe WinForms

---
+++ RecipeApps/RecipeWinForms/SQLUtility.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace CPUFramework
+{
+    public class SQLUtility
+    {
+        public static string ConnectionString = "";
+
+        public static DataTable GetDataTable(string sqlstatement)
+        {
+            DataTable dt = new();
+            SqlConnection conn = new();
+            conn.ConnectionString = ConnectionString;
+            conn.Open();
+            var cmd = new SqlCommand();
+            cmd.CommandText = sqlstatement;
+            cmd.Connection = conn;
+            var dr = cmd.ExecuteReader();
+            dt.Load(dr);
+            return dt;
+        }
+    }
+}
