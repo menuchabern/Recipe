04b60a427baefd3813365bf352edfe09b7697472
menuchabern <menucha9021@gmail.com>
2025-06-16 22:40:09 -0400

    more files changed column cuisinetype to cuisine

--- Database Files/sprocs/RecipeGet.sql
+++ Database Files/sprocs/RecipeGet.sql
@@ -1,13 +1,16 @@
 create or alter procedure dbo.RecipeGet(
 	@All bit = 0,
-	@RecipeName varchar(40)= '',
+	@RecipeName varchar(40)= null,
 	@RecipeID int = 0
 )
 as 
 begin
-	select @RecipeName = nullif(@recipename, '') 
-	select CuisineID, RecipeID, RecipeName, Calories, PictureName, DateDrafted, DatePublished, DateArchived, RecipeStatus
-	from Recipe 
+	select r.recipeid, r.cuisineid, r.usernameid, r.recipename, u.username, r.calories, r.recipestatus, r.datedrafted, r.datearchived, r.datepublished, c.cuisine
+	from recipe r 
+	join username u 
+	on r.usernameid = u.usernameid 
+	join cuisine c 
+	on c.cuisineid = r.cuisineid 
 	where @All = 1
 	or RecipeName like '%' + @RecipeName + '%'
 	or RecipeID = @RecipeID
