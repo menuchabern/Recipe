f96bf88299b42e17d5fcf0e682571907b2000bb4
menuchabern <menucha9021@gmail.com>
2025-11-05 16:40:38 -0500

    in middle of session 2.32 hw

---
+++ RecipeApps/RecipeMAUI/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace RecipeMAUI
+{
+    public partial class App : Application
+    {
+        public static bool LoggedIn = false;
+        public static string ConnStringSetting = "Server=tcp:dev-mb.database.windows.net,1433;Initial Catalog=RecipeDB;Persist Security Info=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
