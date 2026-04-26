814ca4142e2a1b6ea0d73f6444cd902da33e553a
menuchabern <menucha9021@gmail.com>
2026-04-26 13:39:39 -0400

    added bizcookbook and bizmeal

---
+++ RecipeApps/RecipeSystem/bizCookbook.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeSystem
+{
+    public class bizCookbook :bizObject<bizCookbook>
+    {
+        public bizCookbook() { }
+
+        private int _cookbookId;
+        private int _userNameId;
+        private string _cookbookName;
+        private decimal _price;
+        private int _activeStatus;
+        private DateTime _dateCreated;
+        private string _pictureName;
+        private int _skill;
+        private string _skillDesc;
+
+        public List<bizCookbook> Search(string cookbookval)
+        {
+            SqlCommand cmd = SQLUtility.GetSQLCommand(this.GetSprocName);
+            SQLUtility.SetParamValue(cmd, "CookbookName", cookbookval);
+            DataTable dt = SQLUtility.GetDataTable(cmd);
+            return this.GetListFromDataTable(dt);
+        }
+
+        public int CookbookId
+        {
+            get { return _cookbookId; }
+            set
+            {
+                if (_cookbookId != value)
+                {
+                    _cookbookId = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+
+        public int UserNameId
+        {
+            get { return _userNameId; }
+            set
+            {
+                if (_userNameId != value)
+                {
+                    _userNameId = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+
+        public string CookbookName
+        {
+            get { return _cookbookName; }
+            set
+            {
+                if (_cookbookName != value)
+                {
+                    _cookbookName = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+
+        public decimal Price
+        {
+            get { return _price; }
+            set
+            {
+                if (_price != value)
+                {
+                    _price = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+
+        public int ActiveStatus
+        {
+            get { return _activeStatus; }
+            set
+            {
+                if (_activeStatus != value)
+                {
+                    _activeStatus = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+
+        public DateTime DateCreated
+        {
+            get { return _dateCreated; }
+            set
+            {
+                if (_dateCreated != value)
+                {
+                    _dateCreated = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+
+        public string PictureName
+        {
+            get { return _pictureName; }
+            set
+            {
+                if (_pictureName != value)
+                {
+                    _pictureName = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+
+        public int Skill
+        {
+            get { return _skill; }
+            set
+            {
+                if (_skill != value)
+                {
+                    _skill = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+
+        public string SkillDesc
+        {
+            get { return _skillDesc; }
+            set
+            {
+                if (_skillDesc != value)
+                {
+                    _skillDesc = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+    }
+}
---
+++ RecipeApps/RecipeSystem/bizMeal.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeSystem
+{
+    public class bizMeal : bizObject<bizMeal>
+    {
+        public bizMeal() { }
+
+        private int _mealId;
+        private int _userNameId;
+        private string _mealName;
+        private DateTime _dateCreated;
+        private int _activestatus;
+        private string _pictureName;
+        private string _mealDesc;
+
+        public List<bizMeal> Search(string mealval) {
+            SqlCommand cmd = SQLUtility.GetSQLCommand(this.GetSprocName);
+            SQLUtility.SetParamValue(cmd, "MealName", mealval);
+            DataTable dt = SQLUtility.GetDataTable(cmd);
+            return this.GetListFromDataTable(dt);
+        }
+
+        public int MealId
+        {
+            get { return _mealId; }
+            set
+            {
+                if (_mealId != value)
+                {
+                    _mealId = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+
+        public int UserNameId
+        {
+            get { return _userNameId; }
+            set
+            {
+                if (_userNameId != value)
+                {
+                    _userNameId = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+
+        public string MealName
+        {
+            get { return _mealName ; }
+            set
+            {
+                if (_mealName != value)
+                {
+                    _mealName = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+
+        public DateTime DateCreated
+        {
+            get { return _dateCreated; }
+            set
+            {
+                if (_dateCreated != value)
+                {
+                    _dateCreated = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+
+        public int ActiveStatus
+        {
+            get { return _activestatus; }
+            set
+            {
+                if (_activestatus != value)
+                {
+                    _activestatus = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+
+        public string PictureName
+        {
+            get { return _pictureName; }
+            set
+            {
+                if (_pictureName != value)
+                {
+                    _pictureName = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+
+        public string MealDesc
+        {
+            get { return _mealName; }
+            set
+            {
+                if (_mealDesc != value)
+                {
+                    _mealDesc = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+    }
+}
